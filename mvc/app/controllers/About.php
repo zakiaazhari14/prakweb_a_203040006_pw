c7118af45da0fbd145a7edc784ff3fd87d7a06ac
Zaki Auliya Azhari <zakiauliyaazhari@gmail.com>
2022-10-10 16:20:26 +0700

    menyelesaikan tugas 5

--- mvc/app/controllers/About.php
+++ mvc/app/controllers/About.php
@@ -1,12 +1,22 @@
 <?php
 
-class About {
-    public function index($nama = 'Zaki', $pekerjaan = 'Mahasiswa')
+class About extends Controller{
+    public function index($nama = 'Zaki', $pekerjaan = 'Mahasiswa', $umur = 20)
     {
-        echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
+    
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ mvc/app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About Me</h1>
+    <p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan'];?>.</p>
